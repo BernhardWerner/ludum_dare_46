4997ff1b485ac3882b3b6dba08dfd7ae5bfe0e90
b_o_werner <werner@ma.tum.de>
2020-04-18 18:58:52 +0200

    Wall jump

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,12 +2,17 @@ extends KinematicBody2D
 class_name Player
 
 export var speed := 400.0
-export var gravity := 1000.0
-export var jump_strength := 530.0
+export var gravity := 1500.0
+export var jump_strength := 650.0
+
+export var wall_push_multiplier := 3
 
 export var acceleration := 0.25
 export var friction := 0.5
 
+export var wall_perception = 20.0
+
+var move_dir := Vector2.ZERO
 var velocity := Vector2.ZERO
 
 ######################### CUSTOM METHODS #########################
@@ -23,14 +28,18 @@ func get_input() -> Vector2:
 func _ready() -> void:
 	yield(owner, "ready")
 	owner.player = self
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y -= jump_strength
-#		elif is_on_wall():
-#			velocity.y -= 0.5 * sqrt(2) * jump_strength
-#			velocity.x -= 0.5 * sqrt(2) * jump_strength * get_input().x
+		elif $FrontWallRay.is_colliding():
+			if move_dir.x == sign($FrontWallRay.cast_to.x):
+				velocity.y = - 0.5 * sqrt(2) * jump_strength
+				velocity.x = - 0.5 * sqrt(2) * jump_strength * move_dir.x
+			else: 
+				$WallJumpBuffer.start()
 
 	##### DEBUG CONTROLS
 	if event is InputEventKey and event.pressed:
@@ -40,21 +49,29 @@ func _input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var move_dir = get_input()
+	move_dir = get_input()
 	
 	if move_dir.x == 0:
 		velocity.x = lerp(velocity.x, 0, friction)
 	else:
 		velocity.x = lerp(velocity.x, move_dir.x * speed, acceleration)
 		$Sprite.flip_h = (-0.5 * move_dir.x + 0.5) as bool
+		$FrontWallRay.cast_to.x = wall_perception * move_dir.x
+		$BackWallRay.cast_to.x = - 1.5 * wall_perception * move_dir.x
 		
-	velocity.y += gravity * delta * (10 if move_dir.y == -1 else 1)
+	# Wall slide
+	var effective_gravity := gravity * delta * (10 if move_dir.y == -1 else 1)
+	if velocity.y > 0 and ($FrontWallRay.is_colliding() or $BackWallRay.is_colliding()) and move_dir.x != 0:
+		effective_gravity *= 0.25
+	velocity.y += effective_gravity
 	
+	if $BackWallRay.is_colliding():
+		if move_dir.x == sign($FrontWallRay.cast_to.x) and Input.is_action_just_pressed("jump") or not $WallJumpBuffer.is_stopped():
+			velocity.y = - 1.0 * jump_strength
+			velocity.x = wall_push_multiplier * jump_strength * move_dir.x
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	print($WallRay.is_colliding())
-			
 
 
 ######################### SIGNALS #########################

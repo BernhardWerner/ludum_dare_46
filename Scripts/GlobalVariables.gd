d2c970f6eb99a5f3ce1e583897d16e1c9e705aec
b_o_werner <werner@ma.tum.de>
2020-04-24 14:51:42 +0200

    Setup of 4 worlds

---
+++ Scripts/GlobalVariables.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var worlds_unlocked := [true, true, true, true]
+var server_numbers  := [1, 1, 1, 1]
+
+######################### CUSTOM METHODS #########################
+
+
+
+######################### BUILT-INS #########################
+
+func _ready() -> void:
+	pass
+
+######################### SIGNALS #########################
--- Scripts/WorldSelect.gd
+++ Scripts/WorldSelect.gd
@@ -2,23 +2,29 @@ extends Control
 
 
 func _ready() -> void:
-	pass
+	var world_buttons := $VBoxContainer/HBoxContainer/WorldButtonContainer.get_children()
+	var server_infos := $VBoxContainer/HBoxContainer/VBoxContainer.get_children() + $VBoxContainer/HBoxContainer/VBoxContainer2.get_children()
+	for i in range(4):
+		if GlobalVariables.worlds_unlocked[i]:
+			world_buttons[i].disabled = false
+			server_infos[i].get_children()[0].texture = preload("res://Graphics/server_00_running.png")
+			server_infos[i].get_children()[1].text = GlobalVariables.server_numbers[i] as String
 
 
 func _on_World1Button_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher.goto_scene("res://Scenes/ThePlain.tscn")
 
 
 func _on_World2Button_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher.goto_scene("res://Scenes/ThePyramid.tscn")
 
 
 func _on_World3Button_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher.goto_scene("res://Scenes/TheRavine.tscn")
 
 
 func _on_World4Button_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher.goto_scene("res://Scenes/TheBolt.tscn")
 
 
 func _on_BackButton_pressed() -> void:

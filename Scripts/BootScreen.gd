828cf880e3892bc2e9ebf53863cae6b18a663d62
b_o_werner <werner@ma.tum.de>
2020-04-24 18:20:29 +0200

    Boot screen

---
+++ Scripts/BootScreen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+######################### CUSTOM METHODS #########################
+
+func go_to_main_menu() -> void:
+	SceneSwitcher.goto_scene("res://Scenes/MainMenu.tscn")
+
+######################### BUILT-INS #########################
+
+func _ready():
+	pass
